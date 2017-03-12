55358e402e90213ffd7f9ac052b003d3c6cb9644
Jay Johnston <jsjjohnston@gmail.com>
2017-03-13 08:36:35 +1100

    Free Image Data

--- INFT566-Project/Project.cpp
+++ INFT566-Project/Project.cpp
@@ -41,6 +41,7 @@ bool Project::startup()
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, imageWidth, imageHeight,	0, GL_RGB, GL_UNSIGNED_BYTE, data);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	stbi_image_free(data); // Free Image Data
 
 	program->compileShader("myShader.vert");
 	program->compileShader("myShader.frag");
