b91dc17cc6c43df9861baba3d41c3c84312cdd9f
Jay Johnston <jsjjohnston@gmail.com>
2017-03-24 08:21:18 +1100

    Added notmal to Vertex

--- INFT566-Project/Primitives/Vertex.h
+++ INFT566-Project/Primitives/Vertex.h
@@ -5,4 +5,5 @@ struct Vertex
 {
 	glm::vec4 position;
 	glm::vec4 colour;
+	glm::vec4 normal;
 };
---
+++ INFT566-Project/ShapeFactory.cpp
@@ -0,0 +1,12 @@
+#include "ShapeFactory.h"
+
+
+
+ShapeFactory::ShapeFactory()
+{
+}
+
+
+ShapeFactory::~ShapeFactory()
+{
+}
